fb28907eced0926fa8808c80c807b8dbd4ca41a5
lmlife <287388482@qq.com>
2018-09-18 23:28:19 +0800

    add readme.md

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+  .g-button {
+    font-size: $font-size;
+    height: $button-height;
+    padding: 0 1em;
+    border-radius: $border-radius;
+    border: 1px solid $border-color;
+    background: $button-bg;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:active {
+      background-color: $button-active-bg;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
