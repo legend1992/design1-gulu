9ca3c5856c42a820ddd1066d8a9415debe4cb99b
lmlife <287388482@qq.com>
2018-09-20 00:22:21 +0800

    添加单元测试

--- src/app.js
+++ src/app.js
@@ -15,3 +15,101 @@ new Vue({
     loading3: false
   }
 })
+
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+
+const expect = chai.expect
+// unit test
+// 1
+{
+  const buttonConstructor = Vue.extend(Button)
+  const button = new buttonConstructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#icon-settings')
+  button.$el.remove()
+  button.$destroy()
+}
+// 2
+{
+  const buttonConstructor = Vue.extend(Button)
+  const button = new buttonConstructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#icon-loading')
+  button.$el.remove()
+  button.$destroy()
+}
+// 3
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const buttonConstructor = Vue.extend(Button)
+  const button = new buttonConstructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'left'
+    }
+  })
+
+  button.$mount(div)
+  let svgElement = button.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('0')
+  button.$el.remove()
+  button.$destroy()
+}
+// 4
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const buttonConstructor = Vue.extend(Button)
+  const button = new buttonConstructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+
+  button.$mount(div)
+  let svgElement = button.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('2')
+  button.$el.remove()
+  button.$destroy()
+}
+// 5
+{
+  const buttonConstructor = Vue.extend(Button)
+  const buttonC = new buttonConstructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  buttonC.$mount()
+  buttonC.$on('click', ()=> {
+    console.log(1)
+  })
+  let button = buttonC.$el
+  let spy = chai.spy(function () {})
+
+  button.click()
+  expect(spy).to.have.been.called()
+  buttonC.$el.remove()
+  buttonC.$destroy()
+}
