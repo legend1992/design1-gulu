4cd130e6e57832331a0a74b990aea4b06609c0eb
lmlife <287388482@qq.com>
2018-09-19 20:30:05 +0800

    添加props:["icon", "iconPosition"]

--- src/button.vue
+++ src/button.vue
@@ -1,26 +1,43 @@
 <template>
-  <button class="g-button">
+  <button class="g-button" :class="{ [`icon-${iconPosition}`]: icon }">
     <svg v-if="icon" class="icon">
       <use :xlink:href="`#icon-${icon}`"></use>
     </svg>
-    <slot>按钮</slot>
+    <div class="content">
+      <slot>按钮</slot>
+    </div>
   </button>
 </template>
 
 <script>
   export default {
-    props: ["icon"]
+    // props: ["icon"]
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(value) {
+          return value==='left' || value==='right'
+        }
+      }
+    }
   }
 </script>
 
 <style lang="scss">
   .g-button {
+    display: inline-flex;
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    cursor: pointer;
+    justify-content: center;
+    align-items: center;
+    vertical-align: top;
     &:hover {
       border-color: var(--border-color-hover);
     }
@@ -30,6 +47,24 @@
     &:focus {
       outline: none;
     }
-    cursor: pointer;
+
+    &.icon-left{
+      > .icon{
+        order: 1;
+        margin-right: .3em;
+      }
+      > .content{
+        order: 2;
+      }
+    }
+    &.icon-right{
+      > .icon{
+        order: 2;
+        margin-left: .3em;
+      }
+      > .content{
+        order: 1;
+      }
+    }
   }
 </style>
