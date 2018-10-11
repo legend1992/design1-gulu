e4eee69c40cccfc73e05e30df41911e4c0d0fd46
lmlife <287388482@qq.com>
2018-10-11 22:25:52 +0800

    发布0.04版

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Button from './src/button'
 import Icon from './src/icon'
 import ButtonGroup from './src/g-button-group'
+import Input from './src/input'
 
-export { Button, Icon, ButtonGroup }
+export { Button, Icon, ButtonGroup, Input }
