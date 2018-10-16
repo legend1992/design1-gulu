f251fa692e53825eea40c0e577c8484c8d8c8cb8
legend1992 <287388482@qq.com>
2018-10-16 11:35:05 +0800

    添加row col组件，雏形

---
+++ src/col.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col" :class="span && [`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      'span': {
+        type: Number
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .col{
+    width: 50%;
+    border: 1px solid red;
+    @for $n from 1 through 24 {
+      &-#{$n} { width: $n/24 * 100%; }
+    }
+  }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped lang="scss">
+  .row{
+    height: 50px;
+    display: flex;
+    margin-top: 5px;
+    text-align: center;
+  }
+</style>
