8cbc3f6d8262d8e6f36442b599910c8791784768
legend1992 <287388482@qq.com>
2018-10-16 14:55:31 +0800

    row col组件offset, gutter功能实现

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="col" :class="span && [`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="colClass" :style="colStyle">
+    <div><slot></slot></div>
   </div>
 </template>
 
@@ -9,6 +9,32 @@
     props: {
       'span': {
         type: Number
+      },
+      'offset': {
+        type: Number
+      }
+    },
+    data (){
+      return {
+        gutter: 0
+      }
+    },
+    computed: {
+      colClass() {
+        let classArr = [];
+        this.span ? classArr.push('col-'+this.span) : "";
+        this.offset ? classArr.push('col-offset-'+this.offset) : "";
+        return classArr
+      },
+      colStyle() {
+        if(this.gutter){
+          return {
+            paddingLeft: this.gutter/2 + 'px',
+            paddingRight: this.gutter/2 + 'px'
+          }
+        }else{
+          return {}
+        }
       }
     }
   }
@@ -17,9 +43,14 @@
 <style scoped lang="scss">
   .col{
     width: 50%;
-    border: 1px solid red;
     @for $n from 1 through 24 {
       &-#{$n} { width: $n/24 * 100%; }
+    }
+    @for $n from 1 through 24 {
+      &-offset-#{$n} { margin-left: $n/24 * 100%; }
+    }
+    > div{
+      border: 1px solid red;
     }
   }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,19 +1,42 @@
 <template>
-  <div class="row">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
-
+    props: {
+      gutter: {
+        type: Number
+      }
+    },
+    mounted(){
+      if(this.gutter){
+        this.$children.forEach($el=> {
+          $el.gutter = this.gutter;
+        })
+      }
+    },
+    computed: {
+      rowStyle() {
+        if(this.gutter){
+          return {
+            marginLeft: -this.gutter/2 + 'px',
+            marginRight: -this.gutter/2 + 'px'
+          }
+        }else{
+          return {}
+        }
+      }
+    }
   }
 </script>
 
 <style scoped lang="scss">
   .row{
-    height: 50px;
     display: flex;
+    /*flex-wrap: wrap;*/
     margin-top: 5px;
     text-align: center;
   }
