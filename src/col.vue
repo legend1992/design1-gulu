ac98a0648be232d6a788092d6f5bd2a553a6119e
legend1992 <287388482@qq.com>
2018-10-16 16:08:03 +0800

    实现响应式

--- src/col.vue
+++ src/col.vue
@@ -1,38 +1,62 @@
 <template>
   <div class="col" :class="colClass" :style="colStyle">
-    <div><slot></slot></div>
+    <div>
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
+  let validator = (value) => {
+    let keys = Object.keys(value);
+    let valid = true;
+    keys.forEach(key => {
+      if (!['span', 'offset'].includes(key)) {
+        valid = false
+      }
+    });
+    return valid
+  };
   export default {
     props: {
-      span: {
-        type: Number
-      },
-      offset: {
-        type: Number
-      }
+      span: { type: Number },
+      offset: { type: Number },
+      phone: { type: Object, validator },
+      ipad: { type: Object, validator },
+      narrowPc: { type: Object, validator },
+      pc: { type: Object, validator },
+      widePc: { type: Object, validator }
     },
-    data (){
+    data() {
       return {
         gutter: 0
       }
     },
     computed: {
       colClass() {
-        let classArr = [];
-        this.span ? classArr.push('col-'+this.span) : "";
-        this.offset ? classArr.push('col-offset-'+this.offset) : "";
-        return classArr
+        let {span, offset, phone, ipad, narrowPc, pc, widePc} = this;
+        return [
+          span && 'col-' + span,
+          offset && 'col-offset-' + offset,
+          phone && phone.span && 'col-phone-' + phone.span,
+          phone && phone.offset && 'col-phone-offset-' + phone.offset,
+          ipad && ipad.span && 'col-ipad-' + ipad.span,
+          ipad && ipad.offset && 'col-ipad-offset-' + ipad.offset,
+          narrowPc && narrowPc.span && 'col-narrowPc-' + narrowPc.span,
+          narrowPc && narrowPc.offset && 'col-narrowPc-offset-' + narrowPc.offset,
+          pc && pc.span && 'col-pc-' + pc.span,
+          pc && pc.offset && 'col-pc-offset-' + pc.offset,
+          widePc && widePc.span && 'col-widePc-' + widePc.span,
+          widePc && widePc.offset && 'col-widePc-offset-' + widePc.offset
+        ]
       },
       colStyle() {
-        if(this.gutter){
+        if (this.gutter) {
           return {
-            paddingLeft: this.gutter/2 + 'px',
-            paddingRight: this.gutter/2 + 'px'
+            paddingLeft: this.gutter / 2 + 'px',
+            paddingRight: this.gutter / 2 + 'px'
           }
-        }else{
+        } else {
           return {}
         }
       }
@@ -41,14 +65,45 @@
 </script>
 
 <style scoped lang="scss">
-  .col{
+  @mixin set-col-widthAndOffset($name) {
     @for $n from 1 through 24 {
-      &-#{$n} { width: $n/24 * 100%; }
+      &.col-#{$name}-#{$n} {
+        width: ($n / 24) * 100%;
+      }
     }
     @for $n from 1 through 24 {
-      &-offset-#{$n} { margin-left: $n/24 * 100%; }
+      &.#{$name}-offset-#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
     }
-    > div{
+  }
+  .col {
+    @for $n from 1 through 24 {
+      &-#{$n} {
+        width: $n/24 * 100%;
+      }
+    }
+    @for $n from 1 through 24 {
+      &-offset-#{$n} {
+        margin-left: $n/24 * 100%;
+      }
+    }
+    @media (max-width: 1600px) {
+      @include set-col-widthAndOffset(widePc);
+    }
+    @media (max-width: 1200px) {
+      @include set-col-widthAndOffset(pc);
+    }
+    @media (max-width: 992px) {
+      @include set-col-widthAndOffset(narrowPc);
+    }
+    @media (max-width: 768px) {
+      @include set-col-widthAndOffset(ipad);
+    }
+    @media (max-width: 576px) {
+      @include set-col-widthAndOffset(phone);
+    }
+    > div {
       border: 1px solid red;
     }
   }
