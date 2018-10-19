990c782724903d3164f517394b4bf1d2c897270e
legend1992 <287388482@qq.com>
2018-10-19 19:00:56 +0800

    布局组件初步

---
+++ src/content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="content">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "g-content"
+  }
+</script>
+
+<style scoped lang="scss">
+  .content{
+    flex: 1;
+  }
+</style>
