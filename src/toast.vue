eae9459999da940f128025a4e59f33580bf95beb
legend1992 <287388482@qq.com>
2018-10-22 11:05:47 +0800

    toast组件完善

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="toast"></div>
+  <div class="toast">
+    <slot></slot>
+    <!--<div></div>-->
+    <span class="closeButton">关闭</span>
+  </div>
 </template>
 
 <script>
@@ -8,6 +12,22 @@
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  $font-size: 14px;
+  $toast-bg: rgba(0, 0, 0, 0.75);
+  .toast{
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: .8em 1em;
+    background: $toast-bg;
+    border-radius: 4px;
+    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+    color: #fff;
+    font-size: $font-size;
+    .closeButton{
+      display: inline-block;
+    }
+  }
 </style>
