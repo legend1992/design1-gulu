014d7f66b0e4320e127e617cc855cd055a63f6ff
legend1992 <287388482@qq.com>
2018-10-22 11:38:57 +0800

    toast组件样式，点击事件完善

--- src/plugin.js
+++ src/plugin.js
@@ -3,8 +3,16 @@ export default {
   install (Vue, options) {
     Vue.prototype.$toast = function (message) {
       let Constructor = Vue.extend(Toast);
-      let toast = new Constructor();
-      console.log(message);
+      let toast = new Constructor({
+        propsData: {
+          closeButton: {
+            text: '知道了',
+            callback: ()=> {
+              console.log('aasdfsdf')
+            }
+          }
+        }
+      });
       toast.$slots.default = [message];
       toast.$mount();
       document.body.appendChild(toast.$el);
--- src/toast.vue
+++ src/toast.vue
@@ -1,14 +1,55 @@
 <template>
   <div class="toast">
-    <slot></slot>
-    <!--<div></div>-->
-    <span class="closeButton">关闭</span>
+    <div class="message">
+      <slot></slot>
+    </div><span v-if="closeButton" class="closeButton" @click="closeToast">{{closeButton.text}}</span>
   </div>
 </template>
 
 <script>
   export default {
-    name: "toast"
+    name: "toast",
+    props: {
+      autoClose: {
+        type: Boolean,
+        default: true
+      },
+      autoCloseDelay: {
+        type: Number,
+        default: 50000
+      },
+      closeButton: {
+        type: Object,
+        default: ()=> {
+          return {
+            text: '关闭',
+            callback: (toast)=> {
+              toast.close()
+            }
+          }
+        }
+      }
+    },
+    created(){
+      console.log(this.closeButton);
+    },
+    mounted(){
+      if(this.autoClose) {
+        setTimeout(()=>{
+          this.close()
+        },this.autoCloseDelay * 1000)
+      }
+    },
+    methods: {
+      close() {
+        this.$el.remove();
+        this.$destroy();
+      },
+      closeToast(){
+        this.close();
+        this.closeButton.callback();
+      }
+    }
   }
 </script>
 
@@ -20,12 +61,26 @@
     top: 0;
     left: 50%;
     transform: translateX(-50%);
-    padding: .8em 1em;
+    padding: 0 1em;
     background: $toast-bg;
     border-radius: 4px;
     box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
     color: #fff;
     font-size: $font-size;
+    .message{
+      display: inline-block;
+      position: relative;
+      padding: .8em 1em .8em 0;
+      margin-right: 1em;
+      &::after{
+        content: '';
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        border-right: 1px solid #666;
+      }
+    }
     .closeButton{
       display: inline-block;
     }
